f95504448e6b599f60e7ff64027121d64d7e1254
IvanRomero03 <i.wells.ar@gmail.com>
2022-10-20 13:14:12 -0500

    PoseDetector

--- catkin_ws/src/vision/src/human_detection/src/pose_detection.py
+++ catkin_ws/src/vision/src/human_detection/src/pose_detection.py
@@ -8,6 +8,40 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from vision.msg import pose_point
 
+indexToName = ["nose",
+               "leftEyeInner",
+               "leftEye",
+               "leftEyeOuter",
+               "rightEyeInner",
+               "rightEye",
+               "rightEyeOuter",
+               "leftEar",
+               "rightEar",
+               "mouthLeft",
+               "mouthRight",
+               "leftShoulder",
+               "rightShoulder",
+               "leftElbow",
+               "rightElbow",
+               "leftWrist",
+               "rightWrist",
+               "leftPinky",
+               "rightPinky",
+               "leftIndex",
+               "rightIndex",
+               "leftThumb",
+               "rightThumb",
+               "leftHip",
+               "rightHip",
+               "leftKnee",
+               "rightKnee",
+               "leftAnkle",
+               "rightAnkle",
+               "leftHeel",
+               "rightHeel",
+               "leftFootIndex",
+               "rightFootIndex"]
+
 
 class PoseDetector:
     def __init__(self):
@@ -19,85 +53,43 @@ class PoseDetector:
         rospy.init_node('PoseDetector')
 
         self.imageSub = rospy.Subscriber(
-            "/hsrb/head_center_camera/image_raw", Image, image_callback)
+            "/hsrb/head_center_camera/image_raw", Image, self.image_callback)
 
         self.posePub = rospy.Publisher(
             "/hsrb/pose", pose_point, queue_size=10)
 
+    def image_callback(self, data):
+        self.imageReceved = data
 
-# IMG CONFIGS
-WHITE_COLOR = (224, 224, 224)
-color: Tuple[int, int, int] = WHITE_COLOR
-thickness: int = 2
-circle_radius: int = 2
+    def run(self):
+        with self.mp_pose.Pose(
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as pose:
+            while True:
+                if imageReceved is not None:
+                    image = self.bridge.imgmsg_to_cv2(imageReceved, "rgb8")
 
-circle_border_radius = max(circle_radius + 1,
-                           int(circle_radius * 1.2))
+                    image.flags.writeable = False
+                    results = pose.process(image)
+
+                    if results.pose_landmarks:
+                        x = (
+                            results.pose_landmarks.landmark[12].x + results.pose_landmarks.landmark[11].x) / 2
+                        y = (
+                            results.pose_landmarks.landmark[12].y + results.pose_landmarks.landmark[11].y) / 2
+                        z = (
+                            results.pose_landmarks.landmark[12].z + results.pose_landmarks.landmark[11].z) / 2
+                        for(i, landmark) in enumerate(results.pose_landmarks.landmark):
+                            self.posePub.publish(
+                                indexToName[i], landmark.x, landmark.y, landmark.z)
+                            print(indexToName[i])
+                            print("x: ", x, "y: ", y, "z: ", z)
+                        print("Chest: ")
+                        print("x: ", x, "y: ", y, "z: ", z)
+                        self.posePub.publish("chest", x, y, z)
+                else:
+                    print("Image not recived")
+                sleep(1)
 
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
-imageReceved = None
-
-bridge = CvBridge()
-
-
-def image_callback(data):
-    global imageReceved
-    print("Recived cam")
-    imageReceved = data
-
-
-rospy.init_node('ImageRecever', anonymous=True)
-
-imageSub = rospy.Subscriber(
-    "/hsrb/head_center_camera/image_raw", Image, image_callback)
-
-# rosbag play 2022-10-06-19-23-37-09.bag
-
-with mp_pose.Pose(
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as pose:
-    while True:
-        if imageReceved is not None:
-            image = bridge.imgmsg_to_cv2(imageReceved, "rgb8")
-
-            image.flags.writeable = False
-            results = pose.process(image)
-
-            # print(results.pose_landmarks.landmark[12])
-            # print(results.pose_landmarks.landmark[11])
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            if results.pose_landmarks:
-                x = (
-                    results.pose_landmarks.landmark[12].x + results.pose_landmarks.landmark[11].x) / 2
-                y = (
-                    results.pose_landmarks.landmark[12].y + results.pose_landmarks.landmark[11].y) / 2
-                z = (
-                    results.pose_landmarks.landmark[12].z + results.pose_landmarks.landmark[11].z) / 2
-
-                mp_drawing.draw_landmarks(
-                    image,
-                    results.pose_landmarks,
-                    mp_pose.POSE_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-
-                cv2.circle(image,
-                           (int((results.pose_landmarks.landmark[12].x+results.pose_landmarks.landmark[11].x)/2 * 640), int(
-                               (results.pose_landmarks.landmark[12].y + results.pose_landmarks.landmark[11].y)/2*480)), circle_radius, WHITE_COLOR,
-                           thickness)
-                cv2.circle(image,
-                           (int((results.pose_landmarks.landmark[12].x+results.pose_landmarks.landmark[11].x)/2 * 640), int(
-                               (results.pose_landmarks.landmark[12].y + results.pose_landmarks.landmark[11].y)/2*480)),
-                           circle_border_radius, (0, 0, 255),
-                           thickness + 1)
-
-                cv2.imshow('MediaPipe Pose', image)
-                if cv2.waitKey(5) & 0xFF == 27:
-                    break
-        else:
-            print("Image not recived")
-        sleep(1)
+PoseDetector().run()
