0f3657696e24544f8802d2fd24829149b8ed89ee
BryanMqz <A01562119@tec.mx>
2022-10-19 23:15:52 -0500

    Changemap without costmap

--- catkin_ws/src/navigation/nav_main/src/changeMap.cpp
+++ catkin_ws/src/navigation/nav_main/src/changeMap.cpp
@@ -6,6 +6,7 @@
 #include <nav_msgs/MapMetaData.h>
 #include <nav_msgs/LoadMap.h>
 #include <std_msgs/String.h>
+#include <std_srvs/Empty.h>
 
 boost::shared_ptr<nav_msgs::OccupancyGrid const> map_;
 std_msgs::String mapID;
@@ -19,27 +20,33 @@ void mapCallback(const boost::shared_ptr<nav_msgs::OccupancyGrid const>& map){
    }
 int main(int argc, char **argv)
 {
+  bool enter = true;
   ros::init(argc, argv, "Roborregos_map_change");
 
   ros::NodeHandle n;
+  ros::ServiceClient clear_costmap;
 
   ros::Subscriber sub_map = n.subscribe("map", 1000, mapCallback);
   ros::Subscriber sub_mapID = n.subscribe("mapID", 1000, mapIDCallback);
+  clear_costmap = n.serviceClient<std_srvs::Empty>("/move_base/clear_costmaps");
   ros::Rate loop_rate(10);
   while (ros::ok())
   {
     //Receive Type of map
-    if(mapID.data=="LayoutA"){
-        // Now change the map
-        nav_msgs::LoadMap::Request  req;
-        nav_msgs::LoadMap::Response resp;
-        req.map_url = ros::package::getPath("map_server") + "maps/roborregosmapB.yaml";
-        ros::service::waitForService("change_map", 5000);
-        ros::service::call("change_map", req, resp);
-
-        //Put the old map back so the next test isn't broken
-        req.map_url = ros::package::getPath("map_server") + "maps/roborregosmapA.yaml";
-        ros::service::call("change_map", req, resp);
+    if(mapID.data=="LayoutA" && enter == true){
+      std_srvs::Empty emptyCall;
+      ros::service::waitForService("/move_base/clear_costmaps", 10000);
+      clear_costmap.call(emptyCall);
+      // Now change the map
+      nav_msgs::LoadMap::Request  req;
+      nav_msgs::LoadMap::Response resp;
+      req.map_url = ros::package::getPath("nav_main") + "/maps/roborregosmapB.yaml";
+      ros::service::waitForService("change_map", 5000);
+      ros::service::call("change_map", req, resp);
+      //Put the old map back so the next test isn't broken
+      //req.map_url = ros::package::getPath("nav_main") + "/maps/roborregosmap.yaml";
+      //ros::service::call("change_map", req, resp);
+      enter = false;
     }
     else if(mapID.data=="LayoutB"){
         //Change map
